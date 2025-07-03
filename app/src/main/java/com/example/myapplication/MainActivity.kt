f8bf5feb1764fecc3a921dcd1265a96c5edd21ed
Ali <ahmedalielm02@gmail.com>
2025-07-03 18:06:50 +0100

    f

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,14 +15,17 @@ class MainActivity : ComponentActivity() {
 //        enableEdgeToEdge()
         setContent {
 
-            
+            ShowText("Ali")
 
         }
     }
 }
 
 
-
+@Composable
+fun ShowText(name: String) {
+    Text(text = "Hello $name.") // TextView
+}
 
 
 
